61a3e72273c3526826020566280b7f4fe9ff2efb
Michael Asnes <mike.asnes@gmail.com>
2014-07-13 15:17:27 -0600

    added undef routine to hashing.c

--- chap6/hashing.c
+++ chap6/hashing.c
@@ -55,3 +55,57 @@ struct nlist *install(char *name, char *defn)
       return NULL;
    return np;
 }
+
+/* undef: remove name and definition from table */
+void undef(char *name, char *defn)
+{
+   /* What's happening here */
+   /*
+    * if there's a node subsequent to the definition
+    * we want to remove:
+    *    copy its contents to the old definition and remove it.
+    * else:
+    *    just remove the node
+    */
+   struct nlist *np; /* node with our name/definition */
+   struct nlist *todel; /* next node, if any */
+   unsigned hashval;
+
+   if ((np = lookup(name)) == NULL)   /* not found */
+      return;
+
+   if (np->next != NULL) {       /* there's another node in the last after np */
+      todel = np->next;
+      if (todel->name != NULL) {
+         if (np->name == NULL)
+            np->name = malloc(sizeof(todel->name));
+         np->name = strcpy(np->name, todel->name);
+      } else {
+         free(np->name);
+         np->name = NULL;
+      }
+      if (todel->defn != NULL) {
+         if (np->defn == NULL)
+            np->defn = malloc(sizeof(todel->name));
+         strcpy(np->defn, todel->defn);
+      } else {
+         free(np->defn);
+         np->defn = NULL;
+      }
+      if (todel->next != NULL)
+         np->next = todel->next;
+      else {
+         np->next = NULL;
+      }
+      free(todel->name);
+      free(todel->defn);
+      free(todel);
+   } else {                /* np is the last node in its list */
+      free(np->name);
+      free(np->defn);
+      free(np->next);
+      free(np);
+   }
+
+   return;
+}
